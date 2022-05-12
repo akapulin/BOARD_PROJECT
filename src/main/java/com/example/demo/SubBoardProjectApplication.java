db21f65bcbf0a0f9b6c3835685b3e4e93fa68d11
akapulin <123_4_@naver.com>
2022-05-12 19:30:14 +0900

    first commit

---
+++ src/main/java/com/example/demo/SubBoardProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubBoardProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubBoardProjectApplication.class, args);
+	}
+
+}
